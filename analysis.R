8a3e97843130e2ed3eb6ebf199f98b5fa77f56fe
Kiran Pradhan <kpradhan@uw.edu>
2018-05-26 13:09:59 -0700

    Fixes #2

---
+++ analysis.R
@@ -0,0 +1,19 @@
+# analysis.R
+# create data frames for visualizations
+library(dplyr)
+
+# hate crimes
+hate_crimes <- read.csv("hate_crimes.csv", stringsAsFactors = FALSE)
+
+# Hate Crimes per State
+
+
+# Factors Influencing Rate of Hate Crimes
+# affect of income inequality on rate of hate crimes
+
+# affect of education on rate of hate crimes
+
+# affect of racial diversity on rate of hate crimes
+
+
+# 2016 Presidential Election
